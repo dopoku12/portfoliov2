817ad7d6bdd8cf634158b07d7a7e569de9be6a17
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-04-16 13:27:22 -0400

    card display changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import Nav from "./components/Nav";
+// import Nav from "./components/Nav";
 import Projects from "./Projects";
 import Resume from "./Resume";
 import AboutMe from "./AboutMe";
+import { useState } from 'react';
+import { FaPaintBrush, FaCode, FaInfo, FaRegNewspaper } from 'react-icons/fa';
 function App() {
+  const [faIcons, setFaIcons] = useState([
+    ///add behavior for click or hover
+    { id: 0, name: 'projects', iconName: FaPaintBrush, },
+    { id: 1, name: 'resume', iconName: FaRegNewspaper, },
+    { id: 2, name: 'Code', iconName: FaCode, },
+    { id: 3, name: 'aboutMe', iconName: FaInfo, }
+  ].map(i => ({ ...i, status: false })
+  )
+  )
+
+  function clickEvent(id) {
+    const change = faIcons.map(i => i.id === id ? { ...i, status: true } : { ...i, status: false })
+    setFaIcons(change)
+    console.log(faIcons[0].status);
+  }
+  faIcons.map(i => console.log(Object.values(i)))
+
+
+
+  const navicons = faIcons.map((i) => {
+    return (
+      <li key={i.id} >
+        <button>
+          <i.iconName onClick={() => clickEvent(i.id)} className='nav-icons' color='5489A8' size={20} />
+          <p>{i.name}</p>
+        </button>
+      </li>
+    )
+  })
+
   return (
     <div className="App">
       <main>
-        {<Projects />}
-        {<Resume />}
-        {<AboutMe />}
+
+        {
+          <Projects />
+        }
+        {
+          faIcons[1].status &&
+          <Resume />
+        }
+        {
+          faIcons[3].status &&
+          <AboutMe />
+
+        }
       </main>
       <footer>
-        {<Nav />}
+        <nav>
+          <ul className='nav-ul'>
+            {
+              navicons
+            }
+
+          </ul>
+        </nav>
+
       </footer>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,11 @@
 
-import { useState } from 'react';
-import { FaPaintBrush, FaCode, FaInfo, FaRegNewspaper } from 'react-icons/fa';
 
-function Nav() {
-  const [faIcons, setFaIcons] = useState([
-    ///add behavior for click or hover
-    { id: 0, name: 'projects', iconName: FaPaintBrush, },
-    { id: 1, name: 'resume', iconName: FaRegNewspaper, },
-    { id: 2, name: 'Code', iconName: FaCode, },
-    { id: 3, name: 'aboutMe', iconName: FaInfo, }
-  ].map(
-    i => (
-      { ...i, status: false }
-    ))
 
-  )
+//   return (
+//     <header>
+     
+//     </header >
+//   );
+// }
 
-  return (
-    <header>
-      <nav>
-        <ul className='nav-ul'>
-          {
-            faIcons.map((i) => {
-              return (
-                <li key={i.id} >
-                  <button>
-                    <i.iconName className='nav-icons' color='5489A8' size={20} />
-                    <p>{i.name}</p>
-                  </button>
-                </li>
-              )
-            })
-          }
-
-        </ul>
-      </nav>
-    </header >
-  );
-}
-
-export default Nav
+// export default Nav
