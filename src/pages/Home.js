52ba7c103e5b3e39b849fc3ea0f30f5fe2e0b9a6
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-05-05 00:23:58 -0400

    adding modes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,54 @@
 import Nav from "../components/Nav"
-import { FaPaintBrush } from 'react-icons/fa'
+import { FaPaintBrush, FaMoon, FaSun } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { useState } from "react"
 function Home() {
+    const [modeHandler, setModeHandler] = useState(
+        [{
+            id: 0,
+            name: 'Moon',
+            iconName: FaMoon,
+            status: true
+        },
+        {
+            id: 1,
+            name: 'Sun',
+            iconName: FaSun,
+            status: false
+        }]
+    )
+
+    function changeMode(id) {
+        modeHandler.map(i => i.id === id ? { ...i, status: true } : null)
+
+    }
+
+    const daynNight = modeHandler.map(i =>
+        <button key={i.id} className={i.name}
+            onClick={() => changeMode(i.id)}>
+            <i.iconName size={25} />
+        </button>
+    )
+
+
     return (
         <div className="home">
             <Nav />
 
+
+
             <main >
+                <div className="mode-container">
+                    {
+                        daynNight
+                    }
+                </div>
                 <header className="intro">
                     <h1 className='intro-h1 animate__animated animate__zoomInDown'> Welcome, I am  David  <span className='last'>Opoku </span>
                     </h1>
                 </header>
-                <p className="intro-pg">
 
+                <p className="intro-pg">
                     A Front-End web Developer/Designer currently based in the Washington DC-Baltimore Area let's come together &
                     <span className="text-link">  Make Something Great</span>
                 </p>
