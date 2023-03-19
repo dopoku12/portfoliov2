86caa2f87bb1346e7253b4f552e60ee9fcec4448
David Opoku <71300793+dopoku12@users.noreply.github.com>
2023-03-19 19:35:49 -0400

    old css removed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,55 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({ faIcons, FaAngleDown }) => {
+    console.log(faIcons);
+
+    const [display, setDisplay] = useState('dark')
     return (
-        <li>
+        <nav color="black">
+            <ul className="flex flex-row 
+           
+            justify-between
+            align-items">
+                {
+                    faIcons.map(i => {
+                        console.log(i);
+                        return (
+                            i.id < 4 ?
+                                <li className="list-none" key={i.id} >
+                                    <a href={i.pathName}>
+                                        <p >
+                                            {i.name}
+                                            <i.iconName size={30} color={i.color} />
+
+                                        </p>
+                                    </a>
+
+                                </li>
+                                :
+                                <li className="list-none">
+                                    <p >
+                                        {i.name}
+                                        <i.iconName size={30} color={i.color} />
+
+                                    </p>
+                                </li>
+                        )
+
+                    })
+                }
+            </ul>
+
             <Link to="/Projects">
                 <footer className="projects-link-container">
-                    <h1 className="projects-link">View Projects</h1>
+                    <h1 className="projects-link">
+                        <FaAngleDown />
+                    </h1>
                 </footer>
             </Link>
-        </li>
-    );
+
+        </nav>
+    )
 }
 
 export default Nav;
