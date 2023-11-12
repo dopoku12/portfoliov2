fcd0d92d072002d6d2118d4982c1621d9b4f0d0a
dopoku12 <davidopoku30@gmail.com>
2023-11-12 01:09:54 -0500

    icon update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,119 +1,149 @@
 import {
     SiBootstrap, SiReact, SiPhp, SiSass, SiGithub,
     SiTailwindcss, SiNodedotjs, SiJirasoftware,
-    SiExpress, SiMongodb, SiMysql,
-    SiMaterialui, SiChakraui,
+    SiExpress, SiMongodb, SiMysql, SiJest, SiVite,
+    SiMaterialui, SiChakraui, SiMicrosoftazure, SiJquery
 } from 'react-icons/si'
 
 function Skills() {
-    let num = 0
     const iconSkills =
         [{
             name: 'Mongodb',
             iconName: SiMongodb,
-            color: '4E9B47 ',
+            fill: '#4E9B47',
+            stroke: '',
+            strokeW: '0'
         },
         {
             name: 'Express.js',
             iconName: SiExpress,
-            color: '4E9B47 ',
+            fill: '#4E9B47 ',
+            stroke: '',
+            strokeW: '0'
         },
         {
             name: 'React.js',
             iconName: SiReact,
-            color: '1de6f8',
+            fill: '#1de6f8',
+            stroke: '',
+            strokeW: '0'
         },
         {
             name: 'Node.js',
             iconName: SiNodedotjs,
-            color: '4E9B47 ',
+            fill: '#4E9B47 ',
+            stroke: '',
+            strokeW: '0'
         },
         {
-            name: 'MySQL',
-            iconName: SiMysql,
-            color: 'skyblue',
+            name: 'Jest.js',
+            iconName: SiJest,
+            fill: 'purple',
+            stroke: '',
+            strokeW: '0'
         },
-
         {
-            name: 'PHP',
-            iconName: SiPhp,
-            color: 'cc6699',
+            name: 'Vite.js',
+            iconName: SiVite,
+            fill: 'orange',
+            stroke: 'white',
+            strokeW: '1'
         },
         {
             name: 'Chakra ui',
             iconName: SiChakraui,
-            color: '78cac3',
+            fill: '#78cac3',
+            stroke: '',
+            strokeW: '0'
         },
 
         {
             name: 'Material ui',
             iconName: SiMaterialui,
-            color: 'blue',
+            fill: '#3399FF',
+            stroke: '',
+            strokeW: '0'
         },
-
-
-
-
         {
-            name: 'Bootstrap',
-            iconName: SiBootstrap,
-            color: '563d7c',
+            name: 'jQuery',
+            iconName: SiJquery,
+            fill: "#3399FF",
+            stroke: '',
+            strokeW: '0'
         },
+
+        // {
+        //     name: 'MySQL',
+        //     iconName: SiMysql,
+        //     fill: 'aqua',
+        //     stroke: '',
+        //     strokeW: '0'
+        // },
+
+        // {
+        //     name: 'PHP',
+        //     iconName: SiPhp,
+        //     fill: '#cc6699',
+        //     stroke: '',
+        //     strokeW: '0'
+        // },
 
         {
             name: 'Sass',
             iconName: SiSass,
-            color: 'cc6699',
+            fill: '#cc6699',
+            stroke: '',
+            strokeW: '0'
+        },
+        {
+            name: 'Bootstrap',
+            iconName: SiBootstrap,
+            fill: '#563d7c',
+            stroke: '',
+            strokeW: '0'
         },
 
         {
             name: 'Tailwind ',
             iconName: SiTailwindcss,
-            color: '78cac3',
+            fill: '#78cac3',
+            stroke: '',
+            strokeW: '0'
         },
 
-
+        {
+            name: 'Azure',
+            iconName: SiMicrosoftazure,
+            fill: '#008AD7',
+            stroke: '',
+            strokeW: '0'
+        },
         {
             name: 'Jira ',
             iconName: SiJirasoftware,
-            color: 'blue',
+            fill: '#0052CC',
+            stroke: '',
+            strokeW: '0'
         },
         {
             name: 'Github',
             iconName: SiGithub,
         },
-        ].map(i => ({ ...i, id: num++ }))
-
-
-    const skillsLi = [
-        { name: 'Agile ' },
-        { name: '' },
-        { name: '' },
-        { name: ' ArcGIS' },
-        { name: 'Unit Testing' },
-
-        { name: 'Seo' },
-        { name: '' },
-    ].map(i => ({ ...i, id: num++ }))
+        ]
 
     return (
         <>
             <header className='text-2xl m-5'>
                 <h2>Skills</h2>
             </header>
-            <ul className='grid grid-cols-4  m-0 gap-10 place-items-center '>
-
+            <ul className='grid grid-cols-4  m-0 gap-10 place-items-center'>
                 {
-                    iconSkills.map(i => (
-                        <li key={i.id}>
-                            {i.name}
-                            <i.iconName size={30} color={i.color} />
+                    iconSkills.map((obj, i) => (
+                        <li key={i}>
+                            {obj.name}
+                            <obj.iconName size={30} fill={obj.fill} stroke={obj.stroke} stroke-width={obj.strokeW} />
                         </li>
                     ))
-                }
-                {
-                    skillsLi.map(i => <li key={i.id}>
-                        {i.name}</li>)
                 }
             </ul>
         </>
